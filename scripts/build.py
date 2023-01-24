c740c20a42267253858316442745e1f8bf47d203
Bhajneet S.K <bhajneet@gmail.com>
2023-01-24 12:37:59 -0500

    build: create instanced and compressed woff2 fonts

--- scripts/build.py
+++ scripts/build.py
@@ -1,8 +1,15 @@
 import subprocess
+from fontTools.ttLib import TTFont
 
 
 def fontmake():
     cmd = ["fontmake", "-g", "./sources/SantLipi.glyphs"]
     variable_output = ["-o", "variable"]
+    interpolate_instances = ["-i", "-o", "ttf"]
     subprocess.run(cmd)  # defaults to otf and ttf
     subprocess.run(cmd + variable_output)
+    subprocess.run(cmd + interpolate_instances)
+
+    variable_font = TTFont("./variable_ttf/SantLipi-VF.ttf")
+    variable_font.flavor = "woff2"
+    variable_font.save("./variable_ttf/SantLipi-VF.woff2")
