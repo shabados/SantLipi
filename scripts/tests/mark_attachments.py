00247eac54430c81f9336704e06e8803644a53d5
Bhajneet S.K <bhajneet@gmail.com>
2023-01-30 16:18:52 -0500

    test: add halant test

--- scripts/tests/mark_attachments.py
+++ scripts/tests/mark_attachments.py
@@ -19,6 +19,11 @@ for vowel in VOWEL_ATTACHMENTS:
         str += f"{letter}{vowel} "
     ma_items.append({"item": str, "description": f"{vowel} attachment"})
 
+str = ""
+for letter in BASE_LETTERS:
+    str += f"{letter}{VIRAMA} "
+ma_items.append({"item": str, "description": f"Virama attachment"})
+
 # show the same akhand on every consonant
 for akhand in BELOW_LETTERS + BELOW_LETTERS_EXTENDED:
     str = ""
@@ -34,28 +39,18 @@ for modifier in BASE_LETTER_MODIFIERS:
     ma_items.append({"item": str, "description": f"{modifier} attachment"})
 
 # for every consonant, show each vowel, akhand, and other modifier as an attachment
-for letter in BASE_LETTERS:
-    str = ""
-    for vowel in VOWEL_ATTACHMENTS:
-        str += f"{letter}{vowel} "
-
-    for akhand in BELOW_LETTERS + BELOW_LETTERS_EXTENDED:
-        str += f"{letter}{VIRAMA}{akhand} "
-
-    for modifier in BASE_LETTER_MODIFIERS + FINAL_MODIFIERS + [VISARGA]:
-        str += f"{letter}{modifier} "
-
-    ma_items.append({"item": str, "description": f"Mark attachments on {letter}"})
-
-for letter in VOWEL_LETTERS:
+for letter in BASE_LETTERS + VOWEL_LETTERS:
     str = f"{letter} "
 
     for vowel in VOWEL_ATTACHMENTS:
         str += f"{letter}{vowel} "
 
+    str += f"{letter}{VIRAMA} "
+
     for akhand in BELOW_LETTERS + BELOW_LETTERS_EXTENDED:
         str += f"{letter}{VIRAMA}{akhand} "
 
     for modifier in BASE_LETTER_MODIFIERS + FINAL_MODIFIERS + [VISARGA]:
         str += f"{letter}{modifier} "
+
     ma_items.append({"item": str, "description": f"Mark attachments on {letter}"})
